28f17b4a2fbdf13c867761d874e05245573a7f5a
Gopal Penny <gopalpenny@gmail.com>
2022-02-05 20:39:46 +0800

    updated fao penman monteith daily

--- R/fao_penman_monteith.R
+++ R/fao_penman_monteith.R
@@ -13,14 +13,342 @@
 #' et0 <- read_et0_csv(et0_path)
 read_et0_csv <- function(et0_path) {
   et0_vars <- readr::read_csv(et0_path, skip = 1, n_max = 1, col_names = FALSE) %>%
-    as.character() %>% ggsub("\\.","") %>% ggsub("Prc","precip")
+    as.character() %>% ggsub("\\.","") %>% ggsub("Prc","precip") %>%
+    ggsub(" ","_") %>%
+    ggsub("[\\(\\)]","")
   et0_units <- readr::read_csv(et0_path, skip = 2, n_max = 1, col_names = FALSE) %>%
-    as.character() %>% ggsub("\\%","pct") %>% ggsub(".C$","degC") #ggsub("°","deg")
+    as.character() %>%
+    ggsub("\\%","pct") %>%
+    ggsub(".C$","degC") %>%
+    ggsub("\\/","_per_") %>%
+    ggsub(" ","_")
   et0_colnames <- paste(et0_vars, et0_units, sep = "_")
   et0_data <- readr::read_csv(et0_path, skip = 3, col_names = et0_colnames)
 
   return(et0_data)
 }
+
+
+#' Extraterrestrial radiation for daily periods, Ra (MJ/m^2/day)
+#'
+#' Get daily extraterrestrial radiation (FAO 56, Ch 3, Eq. 21)
+#' @param lat latitude in decimal degrees
+#' @param date date object or numeric jday
+#' @export
+#' @details
+#' Returns daily extraterrestrial radiation in units of MJ/m^2/day
+#' per FAO 56, Ch 3, Eq. 21. Evaluates the equation
+#'
+#' Ra <- 24 * 60 / pi * Gsc * d_r *
+#'   (w_s * sin(phi) * sin(delta) + cos(phi) * cos(delta) * sin(w_s))
+#'
+#' To do this, it evaluates internal functions to obtain
+#' Gsc solar constant 0.0820, MJ/m^2/day
+#' d_r inverse relative distance Earth-Sun (Equation 23)
+#' w_s sunset hour angle (Equation 25 or 26), rad,
+#' phi latitude (Equation 22), rad
+#' delta solar decimation (Equation 24), rad.
+#' @examples
+#' lat <- -22.9 # Rio de Janeiro
+#' date <- "2019-05-15"
+#' get_Ra_daily(lat, date)
+get_Ra_daily <- function(lat, date) {#}, Gsc = 0.0820, d_r = NULL, w_s = NULL, phi = NULL, delta = NULL) {
+
+  if (is.character(date)) {
+    date <- as.Date(date)
+    jday <- as.numeric(strftime(date, "%j"))
+  } else if (assertthat::is.date(date)) {
+    jday <- as.numeric(strftime(date, "%j"))
+  } else {
+    jday <- jday
+  }
+  Gsc <- 0.0820 # solar constant
+  phi <- get_phi_lat(lat)
+  d_r <- get_d_r(jday)
+  delta <- get_delta(jday)
+  w_s <- get_w_s_angle(lat, date)
+
+  Ra <- 24 * 60 / pi * Gsc * d_r *
+    (w_s * sin(phi) * sin(delta) + cos(phi) * cos(delta) * sin(w_s))
+  return(Ra)
+}
+
+#' Get latitude in rad from decimal degrees (Eq. 22)
+#' @param lat latitude in decimal degrees
+#' @details
+#' Return latitude in rad. Evaluates the equation
+#'
+#' lat * pi / 180
+#' @examples
+#' phi <- get_phi_lat(13.1)
+get_phi_lat <- function(lat) {
+  return(lat * pi / 180)
+}
+
+#' Get inverse relative distance Earth-Sun (Eq 23)
+#' @param date date object or numeric jday
+#' @details
+#' Return inverse relative distance. Evaluates the equation:
+#'
+#' d_r = 1 + 0.033 * cos(2 * pi/ 365 * jday)
+#' @examples
+#' get_d_r("2019-04-15")
+#' get_d_r(105, "%j")
+get_d_r <- function(date) {
+  if (is.character(date)) {
+    date <- as.Date(date)
+    jday <- as.numeric(strftime(date, "%j"))
+  } else if (assertthat::is.date(date)) {
+    jday <- as.numeric(strftime(date, "%j"))
+  } else {
+    jday <- jday
+  }
+  return(1 + 0.033 * cos(2 * pi/ 365 * jday))
+}
+
+#' Get solar decimation (Eq 24)
+#' @param date date object or numeric jday
+#' @details
+#' Return solar decimation in rad. Evaluates the equation:
+#'
+#' delta = 0.409 * sin(2*pi/365 * jday - 1.39)
+#' @examples
+#' get_delta("2019-04-15")
+#' get_delta(105, "%j")
+get_delta <- function(date) {
+  if (is.character(date)) {
+    date <- as.Date(date)
+    jday <- as.numeric(strftime(date, "%j"))
+  } else if (assertthat::is.date(date)) {
+    jday <- as.numeric(strftime(date, "%j"))
+  } else {
+    jday <- jday
+  }
+  return(0.409 * sin(2*pi/365 * jday - 1.39))
+}
+
+
+#' Get sunset hour angle (Eq 25)
+#' @param phi latitude (Equation 22), rad
+#' @param delta solar decimation (Equation 24), rad.
+#' @details
+#' Return solar decimation in rad. Evaluates the equation:
+#'
+#' w_s = acos(-tan(phi) * tan(delta))
+#' @examples
+#' lat <- 13.1
+#' date <- "2019-04-15"
+#' w_s <- get_w_s_angle(lat, date)
+get_w_s_angle <- function(lat,date) {
+  delta <- get_delta(date)
+  phi <- get_phi_lat(lat)
+  w_s <- acos(-tan(phi) * tan(delta))
+  return(w_s)
+}
+
+#' Get daylight hours (Eq. 34)
+#'
+#' @param w_s sunset hour angle (Eq 25)
+#' @export
+#' @details
+#' Return number of daylight hours. Evaluates the equation:
+#'
+#' N = 24 / pi * w_s
+#' @examples
+#' lat <- 13.1
+#' date <- "2019-04-15"
+#' w_s <- get_w_s_angle(lat, date)
+#' get_daylight_hours(w_s)
+get_daylight_hours <- function(w_s) {
+  N <- 24 / pi * w_s
+  return(N)
+}
+
+#' Get solar radiation (Rs)
+#'
+#' Get solar radiation, Rs (FAO 56, Eq. 35) in MJ/m^2/day
+#' @param Ra extraterrestrial radiation, MJ/m^2/day
+#' @param n actual duration of sunshine, hour
+#' @param N maximum possible duration of sunshine or daylight hours, hour
+#' @param as regression constant, expressing the fraction of extraterrestrial
+#'   radiation reaching the earth on overcast days (n = 0)
+#' @param bs increase in solar radiation on clear days relative to overcast
+#'   days, ie, as + bs is the fraction of extraterrestrial radiation reaching
+#'   the earth on clear days (n = N).
+#' @export
+#' @details This function calculates solar or shortwave radiation, in
+#'   MJ/m^2/day. It uses Eq. 35 from FAO 56 in Chapter 3:
+#'
+#'   Rs = (as + bs *  n / N) * Ra
+#'
+#'   Note that \code{n/N} is the relative sunshine duration (unitless). FAO56
+#'   suggests using as = 0.25 and bs = 0.5 if observed values are not available.
+#'   But better to use observations if possible.
+#' @examples
+#' lat <- -22.9 # Rio de Janeiro
+#' date <- "2019-05-15"
+#' n <- 220 / 31 # 220 hours in a month / 31 days
+#' w_s <- get_w_s_angle(lat, date)
+#' N <- get_daylight_hours(w_s)
+#' Ra <- get_Ra_daily(lat, date)
+#' Rs <- get_Rs_daily(Ra, n, N)
+get_Rs_daily <- function(Ra, n, N, as = 0.25, bs = 0.5) {
+  Rs <- (as + bs *  n / N) * Ra
+  return(Rs)
+}
+
+#' Get net shortwave radiation (Rs)
+#'
+#' Get net shortwave radiation, Rs (FAO 56, Eq. 38) in MJ/m^2/day
+#' @param Rs downward shortwave radiation, MJ/m^2/day
+#' @param albedo albedo or canopy reflectance, unitless
+#' @export
+#' @details This function calculates net shortwave radiation, in MJ/m^2/day. It
+#'   uses Eq. 38 from FAO 56 in Chapter 3:
+#'
+#'   Rns = (1 - albedo) * Rs
+#'
+#'   Note that for reference ET0, albedo can be taken as 0.23 for hypothetical
+#'   grass reference crop.
+#' @examples
+#' lat <- -22.9 # Rio de Janeiro
+#' date <- "2019-05-15"
+#' n <- 220 / 31 # 220 hours in a month / 31 days
+#' w_s <- get_w_s_angle(lat, date)
+#' N <- get_daylight_hours(w_s)
+#' Ra <- get_Ra_daily(lat, date)
+#' Rs <- get_Rs_daily(Ra, n, N)
+#' Rns <- get_Rns_daily(Rs, albedo = 0.23)
+get_Rns_daily <- function(Ra, albedo = 0.23) {
+  Rns <- (1 - albedo) * Rs
+  return(Rns)
+}
+
+#' Get clear sky shortwave radiation (Rso)
+#'
+#' Get clear sky shortwave radiation, Rso (FAO 56, Eq. 36) in MJ/m^2/day
+#' @param Ra downward shortwave radiation, MJ/m^2/day
+#' @param z elevation above sea level, m
+#' @details This function calculates clear-sky shortwave radiation, in MJ/m^2/day. It
+#'   uses Eq. 37 from FAO 56 in Chapter 3:
+#'
+#'   Rso = (0.75 + 2e-5*z) * Ra
+#'
+#'   Note that different values for as and bs should be used if available and
+#'   then a different equation (36) can be used.
+#' @examples
+#' lat <- -22.9 # Rio de Janeiro
+#' date <- "2019-05-15"
+#' n <- 220 / 31 # 220 hours in a month / 31 days
+#' w_s <- get_w_s_angle(lat, date)
+#' N <- get_daylight_hours(w_s)
+#' Ra <- get_Ra_daily(lat, date)
+#' Rso <- get_Rso_daily(Ra, z = 100)
+get_Rso_daily <- function(Ra, z) {
+  return((0.75 + 2e-5*z) * Ra)
+}
+
+
+#' Get net outgoing longwave radiation (Rnl)
+#'
+#' Get net outgoing longwave radiation, Rnl (FAO 56, Eq. 39) in MJ/m^2/day
+#' @param Ra downward shortwave radiation, MJ/m^2/day
+#' @param Tmax_K maximum absolute temperature during the 24-hour period, K = deg
+#'   C + 273.16
+#' @param Tmin_K, K minimum absolute temperature during the 24-hour period, K =
+#'   deg C + 273.16
+#' @param ea actual vapour pressure, kPa
+#' @param Rs measured or calculated. (Equation 35) solar radiation [MJ m-2
+#'   day-1],
+#' @param Rso calculated (Equation 36 or 37) clear-sky radiation [MJ m-2 day-1].
+#' @export
+#' @details This function calculates net outgoing longwave radiation, in
+#'   MJ/m^2/day. It uses Eq. 39 from FAO 56 in Chapter 3:
+#'
+#'   Rnl = sigma * (Tmax_K + Tmin_K)/2 * (0.34 - 0.14 * sqrt(e_a)) * (1.35 *
+#'   Rs/Rso - 0.35)
+#'
+#'
+#'   sigma is the Stefan-Boltzmann constant, 4.903e-9 MJ/K^4/m^2/day,
+#'   Rs/Rso relative shortwave radiation (limited to <= 1.0).
+#' @examples
+#' lat <- -22.9 # Rio de Janeiro
+#' date <- "2019-05-15"
+#' n <- 220 / 31 # 220 hours in a month / 31 days
+#' w_s <- get_w_s_angle(lat, date)
+#' N <- get_daylight_hours(w_s)
+#' Ra <- get_Ra_daily(lat, date)
+#' Rso <- get_Rso_daily(Ra, z = 100)
+#' Rs <- get_Rs_daily(Ra, n, N)
+#' Tmax_K <- 25.1+273.16
+#' Tmin_K <- 19.1+273.16
+#' ea <- 2.1
+#' Rnl <- get_Rnl_daily(Ra, Tmax_K, Tmin_K, ea, Rs, Rso)
+get_Rnl_daily <- function(Ra, Tmax_K, Tmin_K, ea, Rs, Rso) {
+  sigma <- 4.903e-9
+  Rnl <- sigma * (Tmax_K^4 + Tmin_K^4)/2 * (0.34 - 0.14 * sqrt(ea)) * (1.35 * Rs/Rso - 0.35)
+  return(Rnl)
+}
+
+
+
+#' Get net radiation (Rn)
+#'
+#' Get net radiation, Rnl (FAO 56, Eq. 40) in MJ/m^2/day
+#' @param lat latitude in decimal degrees
+#' @param date date object or numeric jday
+#' @param Tmax_K maximum absolute temperature during the 24-hour period, K = deg
+#'   C + 273.16
+#' @param Tmin_K, K minimum absolute temperature during the 24-hour period, K =
+#'   deg C + 273.16
+#' @param ea actual vapour pressure, kPa
+#' @param n actual duration of sunshine, hour
+#' @param N maximum possible duration of sunshine or daylight hours, hour
+#' @param as regression constant, expressing the fraction of extraterrestrial
+#'   radiation reaching the earth on overcast days (n = 0)
+#' @param bs increase in solar radiation on clear days relative to overcast
+#'   days, ie, as + bs is the fraction of extraterrestrial radiation reaching
+#'   the earth on clear days (n = N).
+#' @param albedo albedo or canopy reflectance, unitless
+#' @param z elevation above sea level, m
+#' @export
+#' @details This function calculates net radiation, in MJ/m^2/day. It uses Eq.
+#'   40 from FAO 56 in Chapter 3:
+#'
+#'   Rn = Rns - Rnl
+#'
+#'   In this function, Rns and Rnl are automatically calculated via the
+#'   functions:
+#'
+#'   \itemize{ \item{Ra <- get_Ra_daily(lat, date)} \item{Rs <- get_Rs_daily(Ra,
+#'   n, N)} \item{Rso <- get_Rso_daily(Ra, z)} \item{Rns <- get_Rns_daily(Rs,
+#'   albedo)} \item{Rnl <- get_Rnl_daily(Ra, Tmax_K, Tmin_K, ea, Rs, Rso)} }
+#' @examples
+#' lat <- -22.9 # Rio de Janeiro
+#' date <- "2019-05-15"
+#' n <- 220 / 31 # 220 hours in a month / 31 days
+#' w_s <- get_w_s_angle(lat, date)
+#' N <- get_daylight_hours(w_s)
+#' Ra <- get_Ra_daily(lat, date)
+#' Rso <- get_Rso_daily(Ra, z = 100)
+#' Rs <- get_Rs_daily(Ra, n, N)
+#' Tmax_K <- 25.1+273.16
+#' Tmin_K <- 19.1+273.16
+#' ea <- 2.1
+#' Rn <- get_Rn_daily(lat, date, Tmax_K, Tmin_K, ea, n, N, z = 100)
+get_Rn_daily <- function(lat, date, Tmax_K, Tmin_K, ea, n, N, albedo = 0.23, z, as = 0.25, bs = 0.5) {
+
+  Ra <- get_Ra_daily(lat, date)
+  Rs <- get_Rs_daily(Ra, n, N)
+  Rso <- get_Rso_daily(Ra, z)
+  Rns <- get_Rns_daily(Rs, albedo)
+  Rnl <- get_Rnl_daily(Ra, Tmax_K, Tmin_K, ea, Rs, Rso)
+
+  Rn <- Rns - Rnl
+  return(Rn)
+}
+
+
 
 #
 # a <- 0.611
--- tests/testthat/test-fao_penman_monteith.R
+++ tests/testthat/test-fao_penman_monteith.R
@@ -1,19 +1,19 @@
 
 
 
-et0_tbl <- tibble::tibble(`precip_mm/m`=c(10, 12, 21, 14, 14, 175, 356, 338, 219, 48, 8, 13),
-                 `precip_mm/d`=c(0, 0, 1, 0, 0, 6, 11, 11, 7, 2, 0, 0),
-                 `precip cv_pct`=c(181.4, 226.5, 163.9, 126.4, 164, 56.2, 36.3, 48, 53.2, 115.4, 265, 299.8),
-                 `Wet Days_days`=c(0.9, 1, 1.2, 1, 1.3, 8.5, 14.3, 13.3, 8.5, 2.5, 0.7, 0.9),
-                 `Tmp min_degC`=c(13.9, 16.3, 20.2, 25, 28, 26.7, 24.4, 23.8, 23.3, 20.5, 15.9, 12.8),
-                 `Tmp max_degC`=c(29.4, 32.6, 37, 40.9, 42.8, 37.9, 32, 30.7, 32.1, 32.9, 30.7, 28.6),
-                 `Tmp Mean_degC`=c(21.6, 24.4, 28.6, 32.9, 35.4, 32.3, 28.2, 27.2, 27.7, 26.7, 23.3, 20.7),
-                 `Grnd Frost_days`=c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                 `Rel Hum_pct`=c(53.8, 43.5, 30.9, 25.9, 28.9, 55.6, 76.5, 79.6, 74, 61.9, 55.5, 55.8),
-                 `Sun shine_pct`=c(80, 83, 75.9, 76, 72.3, 46.3, 29, 30.3, 48.6, 70.4, 78.1, 79.6),
-                 `Wind (2m)_m/s`=c(1.1, 1.4, 1.4, 1.6, 2.2, 2.4, 2.1, 1.9, 1.4, 1.2, 1.2, 1),
-                 `ETo_mm/m`=c(102, 126, 173, 208, 260, 193, 129, 116, 123, 134, 112, 95),
-                 `ETo_mm/d`=c(3.3, 4.5, 5.6, 6.9, 8.4, 6.4, 4.2, 3.7, 4.1, 4.3, 3.7, 3.1))
+et0_tbl <- tibble::tibble(precip_mm_per_m=c(10, 12, 21, 14, 14, 175, 356, 338, 219, 48, 8, 13),
+                          precip_mm_per_d=c(0, 0, 1, 0, 0, 6, 11, 11, 7, 2, 0, 0),
+                          precip_cv_pct=c(181.4, 226.5, 163.9, 126.4, 164, 56.2, 36.3, 48, 53.2, 115.4, 265, 299.8),
+                          Wet_Days_days=c(0.9, 1, 1.2, 1, 1.3, 8.5, 14.3, 13.3, 8.5, 2.5, 0.7, 0.9),
+                          Tmp_min_degC=c(13.9, 16.3, 20.2, 25, 28, 26.7, 24.4, 23.8, 23.3, 20.5, 15.9, 12.8),
+                          Tmp_max_degC=c(29.4, 32.6, 37, 40.9, 42.8, 37.9, 32, 30.7, 32.1, 32.9, 30.7, 28.6),
+                          Tmp_Mean_degC=c(21.6, 24.4, 28.6, 32.9, 35.4, 32.3, 28.2, 27.2, 27.7, 26.7, 23.3, 20.7),
+                          Grnd_Frost_days=c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                          Rel_Hum_pct=c(53.8, 43.5, 30.9, 25.9, 28.9, 55.6, 76.5, 79.6, 74, 61.9, 55.5, 55.8),
+                          Sun_shine_pct=c(80, 83, 75.9, 76, 72.3, 46.3, 29, 30.3, 48.6, 70.4, 78.1, 79.6),
+                          Wind_2m_m_per_s=c(1.1, 1.4, 1.4, 1.6, 2.2, 2.4, 2.1, 1.9, 1.4, 1.2, 1.2, 1),
+                          ETo_mm_per_m=c(102, 126, 173, 208, 260, 193, 129, 116, 123, 134, 112, 95),
+                          ETo_mm_per_d=c(3.3, 4.5, 5.6, 6.9, 8.4, 6.4, 4.2, 3.7, 4.1, 4.3, 3.7, 3.1))
 
 et0_path <- system.file("extdata", "ET0_example_file.csv", package = "fao56")
 et0 <- read_et0_csv(et0_path)
@@ -24,3 +24,28 @@ test_that("read_et0_csv works", {
   expect_equal(as.matrix(et0), as.matrix(et0_tbl))
 })
 
+
+lat <- -22.9 # Rio de Janeiro
+date <- "2019-05-15"
+n <- 220 / 31 # 220 hours in a month / 31 days
+w_s <- get_w_s_angle(lat, date)
+N <- get_daylight_hours(w_s)
+Tmax_K <- 25.1+273.16
+Tmin_K <- 19.1+273.16
+ea <- 2.1
+
+Ra <- get_Ra_daily(lat, date)
+Rs <- get_Rs_daily(Ra, n, N)
+Rso <- get_Rso_daily(Ra, z = 100)
+Rns <- get_Rns_daily(Rs, albedo)
+Rnl <- get_Rnl_daily(Ra, Tmax_K, Tmin_K, ea, Rs, Rso)
+Rn <- get_Rn_daily(lat, date, Tmax_K, Tmin_K, ea, n, N, z = 100)
+
+test_that("FAO Penman-Monteith works for daily Ra, Rs, Rso, Rns, Rnl, Rn", {
+  expect_equal(round(Ra,5), 25.11103)
+  expect_equal(round(Rs,5), 14.4561)
+  expect_equal(round(Rso,5), 18.88349)
+  expect_equal(round(Rns,5), 11.1312)
+  expect_equal(round(Rnl,5), 3.49445)
+  expect_equal(round(Rn,6), 7.636745)
+})
